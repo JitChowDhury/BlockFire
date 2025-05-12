95e8c5d7759f94c8db208a79e0a0b858044e92bb
JitChowDhury <jitchowdhury8717@gmail.com>
2025-05-12 13:05:20 +0530

    Add Scriptable object for Weapon

--- Assets/Scripts/WeaponSystem/Gun.cs
+++ Assets/Scripts/WeaponSystem/Gun.cs
@@ -6,17 +6,48 @@ public class Gun : MonoBehaviour
     public float damage = 10f;
     public float range = 100f;
     public Camera fpsCam;
+    public WEAPONSO weaponSO;
+
+
+    private float nextTimeToFire = 0f;
+    private bool isShooting = false;
     // Update is called once per frame
     void Update()
     {
-
+        if (isShooting && Time.time >= nextTimeToFire)
+        {
+            nextTimeToFire = Time.time + 1f / weaponSO.FireRate;
+            Shoot();
+        }
     }
 
     public void HandleShoot(InputAction.CallbackContext context)
     {
-        if (!context.performed) return;
-        Shoot();
+        // When button is pressed
+        if (context.started)
+        {
+            if (!weaponSO.isAutomatic)
+            {
+                // Only shoot if cooldown has passed
+                if (Time.time >= nextTimeToFire)
+                {
+                    nextTimeToFire = Time.time + 1f / weaponSO.FireRate;
+                    Shoot();
+                }
+            }
+            else
+            {
+                isShooting = true;
+            }
+        }
+
+        // When button is released
+        else if (context.canceled)
+        {
+            isShooting = false;
+        }
     }
+
 
     void Shoot()
     {
---
+++ Assets/Scripts/WeaponSystem/WEAPONSO.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewCharacterStats", menuName = "Prototypes/CharacterStats", order = 1)]
+public class WEAPONSO : ScriptableObject
+{
+    public float Damage;
+    public float Range;
+    public float FireRate;
+
+    public bool isAutomatic;
+}
