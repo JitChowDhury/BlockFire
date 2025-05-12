09c3b8a3c2cecab5720ad92ac168d6e91f5a8814
JitChowDhury <jitchowdhury8717@gmail.com>
2025-05-12 09:54:14 +0530

    Add Crouch

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -4,9 +4,13 @@ using UnityEngine.InputSystem;
 public class Movement : MonoBehaviour
 {
     private CharacterController playerCharController;
+    private bool isGrounded;
 
-    Vector3 movementDirection;
+    private Vector3 movementDirection;
+    private float verticalVelocity;
     public float speed = 5f;
+    public float jumpForce = 1.5f;
+    public float gravity = -9.8f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -17,21 +21,41 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        isGrounded = playerCharController.isGrounded;
+
         MovePlayer();
+
     }
 
 
     void MovePlayer()
     {
-        playerCharController.Move(transform.TransformDirection(movementDirection) * speed * Time.deltaTime);
+        verticalVelocity += gravity * Time.deltaTime;
+
+        if (playerCharController.isGrounded && verticalVelocity < 0)
+        {
+            verticalVelocity = -0.1f; // Small negative to keep controller grounded
+        }
+
+        Vector3 worldMovementDirection = transform.TransformDirection(movementDirection);
+        worldMovementDirection.y = verticalVelocity;
+        playerCharController.Move(worldMovementDirection * speed * Time.deltaTime);
         Debug.Log(movementDirection);
     }
+
     public void HandleMove(InputAction.CallbackContext context)
     {
 
 
         Vector3 input = context.ReadValue<Vector2>();
         movementDirection = new Vector3(input.x, 0, input.y);
+
+    }
+    public void HandleJump(InputAction.CallbackContext context)
+    {
+
+        if (!context.performed || !isGrounded) return;
+        verticalVelocity = jumpForce;
 
     }
 }
